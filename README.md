626f8e3257e79e0414abc2948a726df6e2e20d1a
Michal Cichowski <michal.cichowski@bytespack.com>
2023-05-26 14:28:34 +0200

    add README
    tye file
    add home controller

---
+++ README.md
@@ -0,0 +1,13 @@
+# BattleShip
+## Req
+
+* .NET 6
+* Project Tye
+
+## Run application
+
+```console
+tye run
+```
+
+Application is available at http://localhost:8000
---
+++ src/BattleShip.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace BattleShip.WebApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> logger;
+
+    public HomeController(ILogger<HomeController> logger)
+        => this.logger = logger;
+
+    [Route(""), HttpGet, ApiExplorerSettings(IgnoreApi = true)]
+    public RedirectResult Index()
+    {
+        this.logger.LogInformation("Redirect to Swagger");
+
+        return this.Redirect("swagger/index.html");
+    }
+}
