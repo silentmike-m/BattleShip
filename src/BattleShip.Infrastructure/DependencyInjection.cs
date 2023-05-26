9301ac5799ef71cecfb70642fe151d4a7689bfd1
Michal Cichowski <michal.cichowski@bytespack.com>
2023-05-26 14:21:50 +0200

    init webapi

---
+++ src/BattleShip.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿namespace BattleShip.Infrastructure;
+
+using System.Reflection;
+using BattleShip.Infrastructure.MemoryDb;
+using BattleShip.Infrastructure.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        services.AddMemoryDb();
+        services.AddBattleshipSwagger(configuration);
+
+        return services;
+    }
+
+    public static void UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseBattleshipSwagger();
+    }
+}
---
+++ src/BattleShip.Infrastructure/Swagger/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿namespace BattleShip.Infrastructure.Swagger;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+internal static class DependencyInjection
+{
+    public static IServiceCollection AddBattleshipSwagger(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.ConfigureSwaggerGen(c =>
+        {
+            c.CustomSchemaIds(s => s.FullName);
+        });
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Battleship",
+                Version = "v1",
+            });
+        });
+
+        return services;
+    }
+
+    public static void UseBattleshipSwagger(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("v1/swagger.json", "Battleship v1");
+            options.OAuthClientId("swagger");
+            options.OAuthUsePkce();
+        });
+    }
+}
