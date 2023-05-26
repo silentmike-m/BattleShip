345717a337331c19ae67e1a8b8187fa092ab847c
Michal Cichowski <michal.cichowski@bytespack.com>
2023-05-26 14:53:58 +0200

    add get game e2e tests

---
+++ src/BattleShip.WebApi/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿namespace BattleShip.WebApi.Controllers;
+
+using BattleShip.Application.Games.Commands;
+using BattleShip.Application.Games.Queries;
+using BattleShip.Application.Games.ViewModel;
+using BattleShip.WebApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController, Route("[controller]/[action]")]
+public sealed class GameController : ApiControllerBase
+{
+    [HttpGet(Name = "GetGame")]
+    public async Task<BaseResponse<Game>> GetGame(CancellationToken cancellationToken = default)
+    {
+        var request = new GetGame();
+
+        var game = await this.Mediator.Send(request, cancellationToken);
+
+        return new BaseResponse<Game>
+        {
+            Response = game,
+        };
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> StartGame(CancellationToken cancellationToken = default)
+    {
+        var request = new StartGame();
+
+        await this.Mediator.Send(request, cancellationToken);
+
+        return this.Ok();
+    }
+}
---
+++ tests/BattleShip.E2ETests/Controllers/GameController/GetGameTests.cs
@@ -0,0 +1,88 @@
+﻿namespace BattleShip.E2ETests.Controllers.GameController;
+
+using System.Net;
+using BattleShip.Application.Commons.Extensions;
+using BattleShip.Application.Exceptions.Games;
+using BattleShip.Application.Games.ViewModel;
+using BattleShip.Domain.Common.Constants;
+using BattleShip.Domain.Entities;
+using BattleShip.Domain.Repositories;
+using BattleShip.WebApi.ViewModels;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+[TestClass]
+public sealed record GetGameTests
+{
+    private const string URL = "Game/GetGame";
+
+    private readonly WebApplicationFactory<Program> factory = new();
+
+    [TestMethod]
+    public async Task Should_Return_Game_Not_Found_When_Missing_Game_On_Get_Game()
+    {
+        //GIVEN
+        var client = this.factory.CreateClient();
+
+        //WHEN
+        var response = await client.GetAsync(URL);
+
+        //THEN
+        response.StatusCode.Should()
+            .Be(HttpStatusCode.InternalServerError)
+            ;
+
+        var responseToString = await response.Content.ReadAsStringAsync();
+
+        var expectedException = new GameNotFoundException();
+
+        var expectedResult = new BaseResponse<Game>
+        {
+            Code = expectedException.Code,
+            Error = expectedException.Message,
+        };
+
+        var result = responseToString.To<BaseResponse<Game>>();
+
+        result.Should()
+            .BeEquivalentTo(expectedResult)
+            ;
+    }
+
+    [TestMethod]
+    public async Task Should_Return_Game_On_Get_Game()
+    {
+        //GIVEN
+        var game = new GameEntity();
+
+        var repository = this.factory.Services.CreateScope().ServiceProvider.GetRequiredService<IGameRepository>();
+        repository.Save(game);
+
+        var client = this.factory.CreateClient();
+
+        //WHEN
+        var response = await client.GetAsync(URL);
+
+        //THEN
+        response.EnsureSuccessStatusCode();
+
+        var responseToString = await response.Content.ReadAsStringAsync();
+
+        var expectedResult = new BaseResponse<Game>
+        {
+            Response = new Game
+            {
+                Columns = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                Rows = CellRowNames.CELL_ROW_MAPPING.Keys.ToList(),
+                Size = GameSize.GAME_MAX_SIZE,
+                Status = game.Status.ToString(),
+            },
+        };
+
+        var result = responseToString.To<BaseResponse<Game>>();
+
+        result.Should()
+            .BeEquivalentTo(expectedResult)
+            ;
+    }
+}
