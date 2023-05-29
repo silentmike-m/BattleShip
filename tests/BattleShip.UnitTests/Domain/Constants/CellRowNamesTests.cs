260f1dd430f0bde8c0894384243343fa3580fd55
Michal Cichowski <michal.cichowski@bytespack.com>
2023-05-29 17:11:19 +0200

    add cell row names mapping unit tests

---
+++ tests/BattleShip.UnitTests/Domain/Constants/CellRowNamesTests.cs
@@ -0,0 +1,33 @@
+﻿namespace BattleShip.UnitTests.Domain.Constants;
+
+using BattleShip.Domain.Common.Constants;
+
+[TestClass]
+public sealed class CellRowNamesTests
+{
+    [TestMethod]
+    public void Cell_Row_Names_Should_Between_Game_Min_And_Max_Size()
+    {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+        CellRowNames.CELL_ROW_MAPPING.Keys.Should()
+            .HaveCount(GameSize.GAME_MAX_SIZE)
+            ;
+    }
+
+    [TestMethod]
+    public void Cell_Row_Names_Should_Contain_Unique_Values()
+    {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+        CellRowNames.CELL_ROW_MAPPING.Values.Should()
+            .OnlyHaveUniqueItems()
+            ;
+    }
+}
